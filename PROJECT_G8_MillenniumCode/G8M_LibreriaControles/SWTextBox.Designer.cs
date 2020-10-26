64c4bb43aa1b2ed1f1af2b25ff29f47e9ce387f0
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2020-10-26 18:53:26 +0100

    SWTextbox (no funciona)

---
+++ PROJECT_G8_MillenniumCode/G8M_LibreriaControles/SWTextBox.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace G8M_LibreriaControles
+{
+    partial class SWTextBox
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_SWtextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_SWtextbox
+            // 
+            this.txt_SWtextbox.Location = new System.Drawing.Point(0, 0);
+            this.txt_SWtextbox.Name = "txt_SWtextbox";
+            this.txt_SWtextbox.Size = new System.Drawing.Size(100, 20);
+            this.txt_SWtextbox.TabIndex = 0;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_SWtextbox;
+    }
+}
