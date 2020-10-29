8ecab3293665a6541b7ab3347d5ab78647204ec1
Yeyizo <ericmoremolao@gmail.com>
2020-10-29 16:07:08 +0100

    hey

--- PROJECT_G8_MillenniumCode/G8M_LibreriaControles/UserControl1.Designer.cs
+++ PROJECT_G8_MillenniumCode/G8M_LibreriaControles/UserControl1.Designer.cs
@@ -28,11 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.txt_codi = new System.Windows.Forms.TextBox();
+            this.txt_descripcio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_codi
+            // 
+            this.txt_codi.Location = new System.Drawing.Point(6, 3);
+            this.txt_codi.Name = "txt_codi";
+            this.txt_codi.Size = new System.Drawing.Size(45, 22);
+            this.txt_codi.TabIndex = 0;
+            this.txt_codi.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // txt_descripcio
+            // 
+            this.txt_descripcio.Enabled = false;
+            this.txt_descripcio.Location = new System.Drawing.Point(57, 3);
+            this.txt_descripcio.Name = "txt_descripcio";
+            this.txt_descripcio.Size = new System.Drawing.Size(226, 22);
+            this.txt_descripcio.TabIndex = 1;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_descripcio);
+            this.Controls.Add(this.txt_codi);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(286, 28);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_codi;
+        private System.Windows.Forms.TextBox txt_descripcio;
     }
 }
--- PROJECT_G8_MillenniumCode/G8M_LibreriaControles/UserControl1.cs
+++ PROJECT_G8_MillenniumCode/G8M_LibreriaControles/UserControl1.cs
@@ -10,11 +10,36 @@ using System.Windows.Forms;
 
 namespace G8M_LibreriaControles
 {
-    public partial class UserControl1: UserControl
+    public partial class UserControl1 : UserControl
     {
+        bool Requerit;
+        string FormCS;
+        string ClasseCS;
+        string Nomid, NomCodi, NomDesc, nomTaula;
+        string controlID;
+
+        public class ValidaCodi
+        {
+            txt_codi.focus;
+        }
+
         public UserControl1()
         {
             InitializeComponent();
+            if (Requerit == true & string.IsNullOrWhiteSpace(txt_codi.Text))
+            {
+                txt_codi.Enabled = false;
+            }
+
+            if (txt_codi.Focus && e.Modifiers == Keys.Control)
+            {
+                FormCS;
+                ClasseCS;
+            }
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
