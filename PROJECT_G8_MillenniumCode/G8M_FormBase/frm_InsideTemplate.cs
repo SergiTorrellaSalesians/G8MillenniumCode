2b4f39fb2d2805de04413b5276524449000f18fb
Sergi Torrella Salesians <sergi.torrella@sarria.salesians.cat>
2020-11-08 22:04:02 +0100

    peta pero masomenos

--- PROJECT_G8_MillenniumCode/G8M_FormBase/frm_InsideTemplate.cs
+++ PROJECT_G8_MillenniumCode/G8M_FormBase/frm_InsideTemplate.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using G8M_AccesoDatos;
 
 namespace G8M_FormBase
 {
 	public partial class frm_InsideTemplate : Form
 	{
+		G8M_AccesoDatos.AccesoDatos ad_lib;
+		public string nomTaula = "Agencies";
+
 		public frm_InsideTemplate()
 		{
 			InitializeComponent();
+		}
+
+		private void frm_InsideTemplate_Load(object sender, EventArgs e)
+		{
+			ad_lib = new AccesoDatos();
+
+			DataSet dtsAgencias = ad_lib.PortarTaula(nomTaula);
+			dtg_BBDDdata.DataSource = dtsAgencias.Tables[0];
+
+			foreach (Control ctrl in this.Controls) {
+				if (ctrl is TextBox)
+				{
+					ctrl.DataBindings.Clear();
+					ctrl.DataBindings.Add("Text", dtsAgencias.Tables[0], ctrl.Tag.ToString());
+				}
+			}
 		}
 	}
 }
