8be19a728ab9e6e0f6af7e5b154b12f79ba0dafa
Yeyizo <ericmoremolao@gmail.com>
2020-11-26 16:48:02 +0100

    mÁS PLANETAS

--- PROJECT_G8_MillenniumCode/G8M_LibreriaFormsTablas/frm_TablaPlanets.Designer.cs
+++ PROJECT_G8_MillenniumCode/G8M_LibreriaFormsTablas/frm_TablaPlanets.Designer.cs
@@ -28,38 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.swTextbox = new G8M_LibreriaControles.SWTextbox();
-            this.SuspendLayout();
-            // 
-            // swTextbox
-            // 
-            this.swTextbox._TableBind = "Planet";
-            this.swTextbox.AllowEmptyText = G8M_LibreriaControles.SWTextbox.EmptyTextbox.Empty;
-            this.swTextbox.InputTextType = G8M_LibreriaControles.SWTextbox.InputType.Text;
-            this.swTextbox.Location = new System.Drawing.Point(291, 185);
-            this.swTextbox.Name = "swTextbox";
-            this.swTextbox.Size = new System.Drawing.Size(100, 22);
-            this.swTextbox.TabIndex = 10;
-            // 
-            // frm_TablaPlanets
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(846, 625);
-            this.Controls.Add(this.swTextbox);
-            this.Name = "frm_TablaPlanets";
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "frm_TablaPlanets";
-            this.Controls.SetChildIndex(this.btn_actualitzar, 0);
-            this.Controls.SetChildIndex(this.btn_value, 0);
-            this.Controls.SetChildIndex(this.richtxt_dataset, 0);
-            this.Controls.SetChildIndex(this.swTextbox, 0);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private G8M_LibreriaControles.SWTextbox swTextbox;
     }
 }
