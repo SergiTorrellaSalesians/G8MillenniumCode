36da830d2d3515d0e94b420ac6136876d5df9343
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2020-11-27 16:54:56 +0100

    Creación de todos los dlls

---
+++ PROJECT_G8_MillenniumCode/G8M_TablaPlanets/frm_TablaPlanets.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace G8M_TablaPlanets
+{
+    partial class frm_TablaPlanets
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.swComboFK3 = new G8M_LibreriaControles.SWComboFK();
+            this.swComboFK4 = new G8M_LibreriaControles.SWComboFK();
+            this.swTextbox4 = new G8M_LibreriaControles.SWTextbox();
+            this.swTextbox5 = new G8M_LibreriaControles.SWTextbox();
+            this.swTextbox6 = new G8M_LibreriaControles.SWTextbox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // swComboFK3
+            // 
+            this.swComboFK3._Display_Member = "DescFiliations";
+            this.swComboFK3._ForeignTable = "Filiations";
+            this.swComboFK3._TableBind = "idFiliation";
+            this.swComboFK3._Value_Member = "idFiliation";
+            this.swComboFK3.FormattingEnabled = true;
+            this.swComboFK3.Location = new System.Drawing.Point(357, 33);
+            this.swComboFK3.Name = "swComboFK3";
+            this.swComboFK3.Size = new System.Drawing.Size(121, 21);
+            this.swComboFK3.TabIndex = 41;
+            // 
+            // swComboFK4
+            // 
+            this.swComboFK4._Display_Member = "CodeSector";
+            this.swComboFK4._ForeignTable = "Sectors";
+            this.swComboFK4._TableBind = "idSector";
+            this.swComboFK4._Value_Member = "idSector";
+            this.swComboFK4.FormattingEnabled = true;
+            this.swComboFK4.Location = new System.Drawing.Point(96, 97);
+            this.swComboFK4.Name = "swComboFK4";
+            this.swComboFK4.Size = new System.Drawing.Size(121, 21);
+            this.swComboFK4.TabIndex = 40;
+            // 
+            // swTextbox4
+            // 
+            this.swTextbox4._TableBind = "parsecs";
+            this.swTextbox4.AllowEmptyField = G8M_LibreriaControles.SWTextbox.EmptyField.WithText;
+            this.swTextbox4.AllowEmptyText = G8M_LibreriaControles.SWTextbox.EmptyTextbox.Empty;
+            this.swTextbox4.InputTextType = G8M_LibreriaControles.SWTextbox.InputType.Text;
+            this.swTextbox4.Location = new System.Drawing.Point(360, 67);
+            this.swTextbox4.Name = "swTextbox4";
+            this.swTextbox4.Size = new System.Drawing.Size(100, 20);
+            this.swTextbox4.TabIndex = 39;
+            // 
+            // swTextbox5
+            // 
+            this.swTextbox5._TableBind = "DescPlanet";
+            this.swTextbox5.AllowEmptyField = G8M_LibreriaControles.SWTextbox.EmptyField.WithText;
+            this.swTextbox5.AllowEmptyText = G8M_LibreriaControles.SWTextbox.EmptyTextbox.Empty;
+            this.swTextbox5.InputTextType = G8M_LibreriaControles.SWTextbox.InputType.Text;
+            this.swTextbox5.Location = new System.Drawing.Point(88, 69);
+            this.swTextbox5.Name = "swTextbox5";
+            this.swTextbox5.Size = new System.Drawing.Size(100, 20);
+            this.swTextbox5.TabIndex = 38;
+            // 
+            // swTextbox6
+            // 
+            this.swTextbox6._TableBind = "CodePlanet";
+            this.swTextbox6.AllowEmptyField = G8M_LibreriaControles.SWTextbox.EmptyField.WithText;
+            this.swTextbox6.AllowEmptyText = G8M_LibreriaControles.SWTextbox.EmptyTextbox.Empty;
+            this.swTextbox6.InputTextType = G8M_LibreriaControles.SWTextbox.InputType.Text;
+            this.swTextbox6.Location = new System.Drawing.Point(87, 42);
+            this.swTextbox6.Name = "swTextbox6";
+            this.swTextbox6.Size = new System.Drawing.Size(100, 20);
+            this.swTextbox6.TabIndex = 37;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(305, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "Parsercs";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(305, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Filiation:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(49, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "Sector:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(49, 67);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 33;
+            this.label9.Text = "Nom:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(49, 42);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 13);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "Codi:";
+            // 
+            // frm_TablaPlanets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 508);
+            this.Controls.Add(this.swComboFK3);
+            this.Controls.Add(this.swComboFK4);
+            this.Controls.Add(this.swTextbox4);
+            this.Controls.Add(this.swTextbox5);
+            this.Controls.Add(this.swTextbox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Name = "frm_TablaPlanets";
+            this.Text = "Form1";
+            this.Controls.SetChildIndex(this.btn_actualitzar, 0);
+            this.Controls.SetChildIndex(this.btn_value, 0);
+            this.Controls.SetChildIndex(this.richtxt_dataset, 0);
+            this.Controls.SetChildIndex(this.label10, 0);
+            this.Controls.SetChildIndex(this.label9, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.swTextbox6, 0);
+            this.Controls.SetChildIndex(this.swTextbox5, 0);
+            this.Controls.SetChildIndex(this.swTextbox4, 0);
+            this.Controls.SetChildIndex(this.swComboFK4, 0);
+            this.Controls.SetChildIndex(this.swComboFK3, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private G8M_LibreriaControles.SWComboFK swComboFK3;
+        private G8M_LibreriaControles.SWComboFK swComboFK4;
+        private G8M_LibreriaControles.SWTextbox swTextbox4;
+        private G8M_LibreriaControles.SWTextbox swTextbox5;
+        private G8M_LibreriaControles.SWTextbox swTextbox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ PROJECT_G8_MillenniumCode/G8M_TablaUsers/frm_TablaUsers.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace G8M_TablaUsers
+{
+    partial class frm_TablaUsers
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbx_rank = new G8M_LibreriaControles.SWComboFK();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_password = new G8M_LibreriaControles.SWTextbox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_username = new G8M_LibreriaControles.SWTextbox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(248, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Rank:";
+            // 
+            // cbx_rank
+            // 
+            this.cbx_rank._Display_Member = "DescRank";
+            this.cbx_rank._ForeignTable = "UserRanks";
+            this.cbx_rank._TableBind = "idUserRank";
+            this.cbx_rank._Value_Member = "idUserRank";
+            this.cbx_rank.FormattingEnabled = true;
+            this.cbx_rank.Location = new System.Drawing.Point(290, 43);
+            this.cbx_rank.Name = "cbx_rank";
+            this.cbx_rank.Size = new System.Drawing.Size(121, 21);
+            this.cbx_rank.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(45, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Password:";
+            // 
+            // txt_password
+            // 
+            this.txt_password._TableBind = "Password";
+            this.txt_password.AllowEmptyField = G8M_LibreriaControles.SWTextbox.EmptyField.WithText;
+            this.txt_password.AllowEmptyText = G8M_LibreriaControles.SWTextbox.EmptyTextbox.Empty;
+            this.txt_password.InputTextType = G8M_LibreriaControles.SWTextbox.InputType.Text;
+            this.txt_password.Location = new System.Drawing.Point(107, 70);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(100, 20);
+            this.txt_password.TabIndex = 18;
+            this.txt_password.Tag = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(43, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Username:";
+            // 
+            // txt_username
+            // 
+            this.txt_username._TableBind = "UserName";
+            this.txt_username.AllowEmptyField = G8M_LibreriaControles.SWTextbox.EmptyField.WithText;
+            this.txt_username.AllowEmptyText = G8M_LibreriaControles.SWTextbox.EmptyTextbox.Empty;
+            this.txt_username.InputTextType = G8M_LibreriaControles.SWTextbox.InputType.Text;
+            this.txt_username.Location = new System.Drawing.Point(107, 44);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(100, 20);
+            this.txt_username.TabIndex = 16;
+            this.txt_username.Tag = "";
+            // 
+            // frm_TablaUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 508);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbx_rank);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_username);
+            this.Name = "frm_TablaUsers";
+            this.Text = "Form1";
+            this.Controls.SetChildIndex(this.btn_actualitzar, 0);
+            this.Controls.SetChildIndex(this.btn_value, 0);
+            this.Controls.SetChildIndex(this.richtxt_dataset, 0);
+            this.Controls.SetChildIndex(this.txt_username, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.txt_password, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.cbx_rank, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private G8M_LibreriaControles.SWComboFK cbx_rank;
+        private System.Windows.Forms.Label label2;
+        private G8M_LibreriaControles.SWTextbox txt_password;
+        private System.Windows.Forms.Label label1;
+        private G8M_LibreriaControles.SWTextbox txt_username;
+    }
+}
+
