5a2d9d7aac8629e0aeec7a05c0bcc7e6572ff174
Yeyizo <ericmoremolao@gmail.com>
2020-12-09 19:10:43 +0100

    imagen database

--- PROJECT_G8_MillenniumCode/G8M_TableUsers/frm_TableUsers.cs
+++ PROJECT_G8_MillenniumCode/G8M_TableUsers/frm_TableUsers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,48 @@ namespace G8M_TableUsers
         {
             InitializeComponent();
         }
+        //public byte[] imageToByteArray(Image i)
+        //{
+        //    using (var ms = new MemoryStream())
+        //    {
+        //        i.Save(ms, i.RawFormat);
+        //        return ms.ToArray();
+        //    }
+        //}
+
+        //public Image byteArrayToImage(byte[] array)
+        //{
+
+        //    using (var ms = new MemoryStream(array))
+        //    {
+        //        return Image.FromStream(ms);
+        //    }
+        //}
+
+        //private void label5_Click(object sender, EventArgs e)
+        //{
+
+        //}
+        //private void OpenImage(bool nuevo)
+        //{
+        //    OpenFileDialog ofd = new OpenFileDialog();
+
+        //    if (ofd.ShowDialog() == DialogResult.OK)
+        //    {
+        //        Image img = new Bitmap(ofd.FileName);
+        //        byte[] imgBytes = imageToByteArray(img);
+
+        //        if (nuevo) row["photo"] = imgBytes;
+        //        else infotabla.Rows[dtgUsers.CurrentCell.RowIndex]["photo"] = imgBytes;
+
+        //        foreach (Control ctr in Controls)
+        //        {
+        //            if (ctr.GetType() == typeof(PictureBox))
+        //            {
+        //                ctr.BackgroundImage = img;
+        //            }
+        //        }
+        //  }
+        //}
     }
 }
