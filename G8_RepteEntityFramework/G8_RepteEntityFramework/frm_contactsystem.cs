7fd55baa56e503ccac55d860e468c5750707a958
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2020-12-11 17:18:08 +0100

    más cambios con otro form

---
+++ G8_RepteEntityFramework/G8_RepteEntityFramework/frm_contactsystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace G8_RepteEntityFramework
+{
+    public partial class frm_contactsystem : Form
+    {
+        public frm_contactsystem()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_contactsystem_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
