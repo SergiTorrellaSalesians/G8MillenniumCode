78ef5ea3ba1eb50eead4f380f2b37619b5771640
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2020-12-11 19:07:51 +0100

    Holllslsss

--- G8_RepteEntityFramework/G8_RepteEntityFramework/Program.cs
+++ G8_RepteEntityFramework/G8_RepteEntityFramework/Program.cs
@@ -16,7 +16,7 @@ namespace G8_RepteEntityFramework
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new frm_contactsystem());
+			Application.Run(new frm_repte());
 		}
 	}
 }
--- G8_RepteEntityFramework/G8_RepteEntityFramework/frm_repte.cs
+++ G8_RepteEntityFramework/G8_RepteEntityFramework/frm_repte.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,8 +13,12 @@ namespace G8_RepteEntityFramework
 {
 	public partial class frm_repte : Form
 	{
-		NewContactEntities db;
+		NewContactEntities db1;
 		List<NewContact> nwc;
+
+		ContactSystemEntities db2;
+		List<ContactSystem> ncent;
+
 		bool EsNou = false;
 
 		public frm_repte()
@@ -23,22 +28,26 @@ namespace G8_RepteEntityFramework
 
 		private void CarregaDades()
         {
-			db = new NewContactEntities();
-			nwc = db.NewContacts.ToList();
+			db1 = new NewContactEntities();
+			nwc = db1.NewContacts.ToList();
+
+			db2 = new ContactSystemEntities();
+			ncent = db2.ContactSystems.ToList();
+
 			dtg_newcontacts.DataSource = nwc;
 			FerBinding();
         }
 
 		private void FerBinding()
         {
-				tbx_idcontact.Clear();
-				tbx_idcontact.DataBindings.Add("Text", nwc, tbx_idcontact.Tag.ToString());
+			tbx_idcontact.Clear();
+			tbx_idcontact.DataBindings.Add("Text", nwc, tbx_idcontact.Tag.ToString());
 
-				tbx_name.Clear();
-				tbx_name.DataBindings.Add("Text", nwc, tbx_name.Tag.ToString());
+			tbx_name.Clear();
+			tbx_name.DataBindings.Add("Text", nwc, tbx_name.Tag.ToString());
 
-				tbx_birthdate.Clear();
-				tbx_birthdate.DataBindings.Add("Text", nwc, tbx_birthdate.Tag.ToString());
+			tbx_birthdate.Clear();
+			tbx_birthdate.DataBindings.Add("Text", nwc, tbx_birthdate.Tag.ToString());
 		}
 
 		private void TreuBinding()
@@ -54,27 +63,63 @@ namespace G8_RepteEntityFramework
 		}
         private void frm_repte_Load(object sender, EventArgs e)
         {
-            // TODO: esta línea de código carga datos en la tabla 'secureCoreDataSet.NewContact' Puede moverla o quitarla según sea necesario.
-            //this.newContactTableAdapter.Fill(this.secureCoreDataSet.NewContact);
+			
+
+
 			CarregaDades();
 
         }
 
         private void btn_update_Click(object sender, EventArgs e)
         {
-            if (EsNou)
-            {
-				NewContact newcont = new NewContact
+			string projDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+			string edi = System.IO.File.ReadAllText(projDir + "\\DadesClients.edi");
+
+			string[] subsEdi = edi.Split('|');
+			int actualContact = 0;
+			for (int i = 0; i < subsEdi.Length;)
+			{
+				if (subsEdi[i].Trim() == "CON") // IDcontact, name, birth
 				{
-					//idContact = int.Parse(tbx_idcontact.Text),
-					Name = tbx_name.Text,
-					BirthDate = tbx_birthdate.Text
+					//MessageBox.Show("CON :" + subsEdi[i + 1] + " # " + subsEdi[i + 2] + " # " + subsEdi[i + 3]); //
 
-				};
-				db.NewContacts.Add(newcont);
-            }
+					if (EsNou)
+					{
+						NewContact newcont = new NewContact
+						{
+							idContact = actualContact,
+							Name = subsEdi[i + 1],
+							BirthDate = subsEdi[i + 2]
+						};
+						db1.NewContacts.Add(newcont);
+					}
+					db1.SaveChanges();
 
-			db.SaveChanges();
+					i += 4;
+					actualContact++;
+				}
+				else if (subsEdi[i].Trim() == "SYS") // IDcontact, description, value
+				{
+					//MessageBox.Show("SYS :" + subsEdi[i + 1] + " # " + subsEdi[i + 2]); //
+					
+					if (EsNou)
+					{
+						ContactSystem ctcsys = new ContactSystem
+						{
+							IdContact = actualContact,
+							Description = subsEdi[i + 1],
+							SystemValue = subsEdi[i + 2]
+						};
+						db2.ContactSystems.Add(ctcsys);
+					}
+					db2.SaveChanges();
+
+					i += 3;
+				}
+			}
+
+
+			
         }
 		//
         private void btn_newproduct_Click(object sender, EventArgs e)
