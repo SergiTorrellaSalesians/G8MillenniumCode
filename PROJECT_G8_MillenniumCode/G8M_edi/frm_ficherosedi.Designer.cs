d1f76c878751736fefd8b19673808d448b3b9188
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2021-01-13 19:22:00 +0100

    Revert "Seleccionar, subir y bajar archivo FTP"
    This reverts commit acccc72fab0c6a963660fab7e10754a87b80b5c3.

---
+++ PROJECT_G8_MillenniumCode/G8M_edi/frm_ficherosedi.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace G8M_PantallaBuscador
+{
+	partial class frm_buscador
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.btn_generaredi = new System.Windows.Forms.Button();
+            this.txbSelectedFile = new System.Windows.Forms.TextBox();
+            this.dtgFiles = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_generaredi
+            // 
+            this.btn_generaredi.Location = new System.Drawing.Point(169, 202);
+            this.btn_generaredi.Name = "btn_generaredi";
+            this.btn_generaredi.Size = new System.Drawing.Size(75, 23);
+            this.btn_generaredi.TabIndex = 1;
+            this.btn_generaredi.Text = "Generar EDI";
+            this.btn_generaredi.UseVisualStyleBackColor = true;
+            this.btn_generaredi.Click += new System.EventHandler(this.btn_generaredi_Click);
+            // 
+            // txbSelectedFile
+            // 
+            this.txbSelectedFile.Location = new System.Drawing.Point(116, 153);
+            this.txbSelectedFile.Margin = new System.Windows.Forms.Padding(2);
+            this.txbSelectedFile.Name = "txbSelectedFile";
+            this.txbSelectedFile.Size = new System.Drawing.Size(171, 20);
+            this.txbSelectedFile.TabIndex = 4;
+            // 
+            // dtgFiles
+            // 
+            this.dtgFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgFiles.Location = new System.Drawing.Point(341, 86);
+            this.dtgFiles.Margin = new System.Windows.Forms.Padding(2);
+            this.dtgFiles.Name = "dtgFiles";
+            this.dtgFiles.RowHeadersWidth = 51;
+            this.dtgFiles.RowTemplate.Height = 24;
+            this.dtgFiles.Size = new System.Drawing.Size(180, 202);
+            this.dtgFiles.TabIndex = 5;
+            // 
+            // frm_buscador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(658, 393);
+            this.Controls.Add(this.dtgFiles);
+            this.Controls.Add(this.txbSelectedFile);
+            this.Controls.Add(this.btn_generaredi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_buscador";
+            this.Text = "frm_buscador";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_generaredi;
+        private System.Windows.Forms.TextBox txbSelectedFile;
+        private System.Windows.Forms.DataGridView dtgFiles;
+    }
+}
--- PROJECT_G8_MillenniumCode/G8_MillenniumCode/frm_pantallaInicio.Designer.cs
+++ PROJECT_G8_MillenniumCode/G8_MillenniumCode/frm_pantallaInicio.Designer.cs
@@ -28,39 +28,52 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.BackgroundImage = global::G8_MillenniumCode.Properties.Resources.noms;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 107);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(522, 126);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 6;
-            this.pictureBox2.TabStop = false;
-            // 
-            // frm_pantallaInicio
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::G8_MillenniumCode.Properties.Resources.istockphoto_606667670_612x612;
-            this.ClientSize = new System.Drawing.Size(546, 353);
-            this.Controls.Add(this.pictureBox2);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "frm_pantallaInicio";
-            this.Text = "frm_pantallaInicio";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.ResumeLayout(false);
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox2.BackgroundImage = global::G8_MillenniumCode.Properties.Resources.noms;
+			this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox2.Location = new System.Drawing.Point(12, 107);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(522, 126);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 6;
+			this.pictureBox2.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(242, 267);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// frm_pantallaInicio
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::G8_MillenniumCode.Properties.Resources.istockphoto_606667670_612x612;
+			this.ClientSize = new System.Drawing.Size(546, 353);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox2);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "frm_pantallaInicio";
+			this.Text = "frm_pantallaInicio";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Button button1;
 	}
 }
