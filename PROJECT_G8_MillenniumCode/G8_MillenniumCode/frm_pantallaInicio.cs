b7b408544c780283aa85e5726f885d0e5187cfd6
Sergi Torrella Salesians <sergi.torrella@sarria.salesians.cat>
2021-01-13 16:00:36 +0100

    routeedi

--- PROJECT_G8_MillenniumCode/G8_MillenniumCode/frm_pantallaInicio.cs
+++ PROJECT_G8_MillenniumCode/G8_MillenniumCode/frm_pantallaInicio.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace G8_MillenniumCode
 {
@@ -16,5 +17,28 @@ namespace G8_MillenniumCode
 		{
 			InitializeComponent();
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+            string fileContent = "";
+            string filePath = "";
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.InitialDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+                openFileDialog.Filter = "Edi files (*.edi)|*.edi" +"|"+ "All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    filePath = openFileDialog.FileName;
+                    Stream fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream)) {
+                        fileContent = reader.ReadToEnd();
+                    }
+                    MessageBox.Show(fileContent, "FILE: " + filePath, MessageBoxButtons.OK);
+                }
+            }
+        }
 	}
 }
