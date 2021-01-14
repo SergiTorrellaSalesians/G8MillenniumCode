f84b9810ba6f561b86aaf11eb500d97fd8b7c3ba
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2021-01-14 20:55:47 +0100

    Cambio visual en formulario Usercard

--- PROJECT_G8_MillenniumCode/G8M_ArchivosEDI/frm_edis.cs
+++ PROJECT_G8_MillenniumCode/G8M_ArchivosEDI/frm_edis.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Configuration;
 using System.Net;
 using CrystalDecisions.CrystalReports.Engine;
 using System.Diagnostics;
--- PROJECT_G8_MillenniumCode/G8M_ConsoleApp/Program.cs
+++ PROJECT_G8_MillenniumCode/G8M_ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+
 namespace G8M_ConsoleApp
 {
     class Program
