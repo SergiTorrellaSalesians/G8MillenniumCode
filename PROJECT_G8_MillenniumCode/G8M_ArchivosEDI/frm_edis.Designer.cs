77936b43a6febcb53924184ec06d4c2615d1de9b
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2021-01-17 19:41:33 +0100

    Cambios CR tabla EDI

--- PROJECT_G8_MillenniumCode/G8M_ArchivosEDI/frm_edis.Designer.cs
+++ PROJECT_G8_MillenniumCode/G8M_ArchivosEDI/frm_edis.Designer.cs
@@ -32,15 +32,19 @@ namespace G8M_ArchivosEDI
             this.btn_selectfile = new System.Windows.Forms.Button();
             this.btn_uploadfile = new System.Windows.Forms.Button();
             this.btn_download = new System.Windows.Forms.Button();
-            this.crviewer_planets = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.btn_crystalreports = new System.Windows.Forms.Button();
             this.btn_showcrystalreports = new System.Windows.Forms.Button();
+            this.tbx_codeOrders = new G8M_LibreriaControles.SWTextbox();
+            this.lbl_codeORder = new System.Windows.Forms.Label();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.EDI_to_crystalreports1 = new G8M_ArchivosEDI.EDI_to_crystalreports();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_selectfile
             // 
-            this.btn_selectfile.Location = new System.Drawing.Point(12, 136);
+            this.btn_selectfile.Location = new System.Drawing.Point(12, 50);
             this.btn_selectfile.Name = "btn_selectfile";
             this.btn_selectfile.Size = new System.Drawing.Size(75, 23);
             this.btn_selectfile.TabIndex = 0;
@@ -50,7 +54,7 @@ namespace G8M_ArchivosEDI
             // 
             // btn_uploadfile
             // 
-            this.btn_uploadfile.Location = new System.Drawing.Point(12, 165);
+            this.btn_uploadfile.Location = new System.Drawing.Point(12, 79);
             this.btn_uploadfile.Name = "btn_uploadfile";
             this.btn_uploadfile.Size = new System.Drawing.Size(75, 23);
             this.btn_uploadfile.TabIndex = 1;
@@ -60,7 +64,7 @@ namespace G8M_ArchivosEDI
             // 
             // btn_download
             // 
-            this.btn_download.Location = new System.Drawing.Point(12, 194);
+            this.btn_download.Location = new System.Drawing.Point(12, 108);
             this.btn_download.Name = "btn_download";
             this.btn_download.Size = new System.Drawing.Size(75, 23);
             this.btn_download.TabIndex = 2;
@@ -68,21 +72,9 @@ namespace G8M_ArchivosEDI
             this.btn_download.UseVisualStyleBackColor = true;
             this.btn_download.Click += new System.EventHandler(this.btn_download_Click);
             // 
-            // crviewer_planets
-            // 
-            this.crviewer_planets.ActiveViewIndex = 0;
-            this.crviewer_planets.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.crviewer_planets.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crviewer_planets.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.crviewer_planets.Location = new System.Drawing.Point(103, 12);
-            this.crviewer_planets.Name = "crviewer_planets";
-            this.crviewer_planets.ReportSource = this.EDI_to_crystalreports1;
-            this.crviewer_planets.Size = new System.Drawing.Size(743, 422);
-            this.crviewer_planets.TabIndex = 3;
-            // 
             // btn_crystalreports
             // 
-            this.btn_crystalreports.Location = new System.Drawing.Point(12, 224);
+            this.btn_crystalreports.Location = new System.Drawing.Point(12, 138);
             this.btn_crystalreports.Name = "btn_crystalreports";
             this.btn_crystalreports.Size = new System.Drawing.Size(75, 23);
             this.btn_crystalreports.TabIndex = 4;
@@ -92,13 +84,56 @@ namespace G8M_ArchivosEDI
             // 
             // btn_showcrystalreports
             // 
-            this.btn_showcrystalreports.Location = new System.Drawing.Point(12, 253);
+            this.btn_showcrystalreports.Location = new System.Drawing.Point(15, 234);
             this.btn_showcrystalreports.Name = "btn_showcrystalreports";
             this.btn_showcrystalreports.Size = new System.Drawing.Size(75, 23);
             this.btn_showcrystalreports.TabIndex = 5;
             this.btn_showcrystalreports.Text = "Mostrar";
             this.btn_showcrystalreports.UseVisualStyleBackColor = true;
             this.btn_showcrystalreports.Click += new System.EventHandler(this.btn_showcrystalreports_Click);
+            // 
+            // tbx_codeOrders
+            // 
+            this.tbx_codeOrders._TableBind = null;
+            this.tbx_codeOrders.AllowEmptyField = G8M_LibreriaControles.SWTextbox.EmptyField.WithText;
+            this.tbx_codeOrders.AllowEmptyText = G8M_LibreriaControles.SWTextbox.EmptyTextbox.Empty;
+            this.tbx_codeOrders.InputTextType = G8M_LibreriaControles.SWTextbox.InputType.Text;
+            this.tbx_codeOrders.Location = new System.Drawing.Point(15, 199);
+            this.tbx_codeOrders.Name = "tbx_codeOrders";
+            this.tbx_codeOrders.Size = new System.Drawing.Size(100, 20);
+            this.tbx_codeOrders.TabIndex = 7;
+            this.tbx_codeOrders.Text = "061243444000";
+            // 
+            // lbl_codeORder
+            // 
+            this.lbl_codeORder.AutoSize = true;
+            this.lbl_codeORder.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_codeORder.ForeColor = System.Drawing.Color.White;
+            this.lbl_codeORder.Location = new System.Drawing.Point(12, 174);
+            this.lbl_codeORder.Name = "lbl_codeORder";
+            this.lbl_codeORder.Size = new System.Drawing.Size(36, 13);
+            this.lbl_codeORder.TabIndex = 25;
+            this.lbl_codeORder.Text = "Order:";
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.EDI_to_crystalreports1;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(725, 404);
+            this.crystalReportViewer1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.crystalReportViewer1);
+            this.panel1.Location = new System.Drawing.Point(121, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 404);
+            this.panel1.TabIndex = 26;
             // 
             // frm_edis
             // 
@@ -107,17 +142,20 @@ namespace G8M_ArchivosEDI
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BackgroundImage = global::G8M_ArchivosEDI.Properties.Resources.istockphoto_606667670_612x612;
             this.ClientSize = new System.Drawing.Size(858, 446);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbl_codeORder);
+            this.Controls.Add(this.tbx_codeOrders);
             this.Controls.Add(this.btn_showcrystalreports);
             this.Controls.Add(this.btn_crystalreports);
             this.Controls.Add(this.btn_download);
             this.Controls.Add(this.btn_uploadfile);
             this.Controls.Add(this.btn_selectfile);
-            this.Controls.Add(this.crviewer_planets);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frm_edis";
             this.Text = "frm_edis";
-            this.Load += new System.EventHandler(this.frm_edis_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -126,9 +164,12 @@ namespace G8M_ArchivosEDI
         private System.Windows.Forms.Button btn_selectfile;
         private System.Windows.Forms.Button btn_uploadfile;
         private System.Windows.Forms.Button btn_download;
-        private CrystalDecisions.Windows.Forms.CrystalReportViewer crviewer_planets;
         private System.Windows.Forms.Button btn_crystalreports;
         private System.Windows.Forms.Button btn_showcrystalreports;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private G8M_LibreriaControles.SWTextbox tbx_codeOrders;
         private EDI_to_crystalreports EDI_to_crystalreports1;
+        private System.Windows.Forms.Label lbl_codeORder;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- PROJECT_G8_MillenniumCode/G8M_ArchivosEDI/frm_edis.cs
+++ PROJECT_G8_MillenniumCode/G8M_ArchivosEDI/frm_edis.cs
@@ -21,7 +21,8 @@ namespace G8M_ArchivosEDI
         public string uploadserverpath = @"/home/g7/";
         public string downloadserverpath = @"/home/g7/";
         public string downloadpath = @"C:/Tractats/";
-        
+
+        public string ordercodetosearch = "";
         public frm_edis()
         {
             InitializeComponent();
@@ -89,80 +90,18 @@ namespace G8M_ArchivosEDI
 
         private void btn_download_Click(object sender, EventArgs e)
         {
-            //string route = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
-            //ProcessStartInfo startInfo = new ProcessStartInfo(route + @"\DLL\G8M_ConsoleApp.exe");
-
-            G8M_ConsoleApp.Program consoleApp = new Program();
 
             ProcessStartInfo startInfo = new ProcessStartInfo("G8M_ConsoleApp.exe");
 
             Process.Start(startInfo);
 
-            //startInfo.Arguments = "header.h";
-
-            //Process.Start(startInfo);
-
-
-            //frm_usercard usercardFRM = new frm_usercard();
-            //usercardFRM.usernameCrystalReports = tbx_username.Text;
-            //usercardFRM.Show();
-
-            //The following code downloads the EDI file from FTP Server using windows form
-            //Process.Start(startInfo);
-            //Process.Start(@"C:\Users\saman\Documents\GitHub\G8MillenniumCode\PROJECT_G8_MillenniumCode\DLL\G8M_ConsoleApp\G8M_ConsoleApp.exe");
-            //if (fileName == "")
-            //{
-            //    MessageBox.Show("No file selected");
-            //} else
-            //{
-            //    string finalpath = downloadserverpath + fileName;
-            //    string finaldownloadpath = downloadpath + fileName;
-            //    //MessageBox.Show(finalpath);
-
-            //    string inputfilepath = @"C:\Tractats\" + fileName;
-            //    string ftphost = serverip;
-            //    string ftpfilepath = downloadserverpath + fileName;
-
-            //    string ftpfullpath = "ftp://" + ftphost + ftpfilepath;
-
-            //    try
-            //    {
-            //        using (WebClient request = new WebClient())
-            //        {
-            //            request.Credentials = new NetworkCredential(user, password);
-            //            byte[] fileData = request.DownloadData(ftpfullpath);
-
-            //            using (FileStream file = File.Create(inputfilepath))
-            //            {
-            //                file.Write(fileData, 0, fileData.Length);
-            //                file.Close();
-            //            }
-            //            MessageBox.Show("Download Complete");
-            //        }
-            //    }
-            //    catch (WebException ex)
-            //    {
-            //        //FtpWebResponse response = (FtpWebResponse)ex.Response;
-            //        //if (response.StatusCode == FtpStatusCode.ActionNotTakenFileUnavailable)
-            //        //{
-
-            //        //}
-            //    }
-            //}
-        }
-
-        private void frm_edis_Load(object sender, EventArgs e)
-        {
-            //crviewer_planets escondido??
-            //EDI_to_crystalreports1.Vis
-                //.Visible = true;
+            startInfo.Arguments = "header.h";
         }
 
         string lines = "";
 
         private void btn_crystalreports_Click(object sender, EventArgs e)
         {
-            //crviewer_planets show
 
             if (fileName == "")
             {
@@ -170,7 +109,6 @@ namespace G8M_ArchivosEDI
             } else
             {
                 string EDIFilepath = downloadpath + fileName;
-                    //"RARROrderSample.edi";
 
                 using (OpenFileDialog openFileDialog = new OpenFileDialog())
                 {
@@ -319,17 +257,7 @@ namespace G8M_ArchivosEDI
 
         private void btn_showcrystalreports_Click(object sender, EventArgs e)
         {
-            if (fileName == "")
-            {
-                MessageBox.Show("No file selected");
-            } else {
-                var cryRpt = new ReportDocument();
-                string route = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
-                //cryRpt.Load(route + @"\G8M_ArchivosEDI\EDI_to_crystalreports.rpt");
-                //crviewer_planets.ReportSource = cryRpt;
-                crviewer_planets.Refresh();
-                //EDI_to_crystalreports1.
-            }
+            EDI_to_crystalreports1.RecordSelectionFormula = "{Comando.codeOrder} = '" + ordercodetosearch + "'";
         }
     }
 }
